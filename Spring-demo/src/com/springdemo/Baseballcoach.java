5887b0893bf55a15dd24cdb2b12b541758163be3
AvinashP <AvinashP@NCATECHM014VD.news.newslimited.local>
2018-08-26 20:55:03 +0530

    First Commit

---
+++ Spring-demo/src/com/springdemo/Baseballcoach.java
@@ -0,0 +1,10 @@
+package com.springdemo;
+
+public class Baseballcoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 min on batting practice";
+	}
+
+}
---
+++ Spring-demo/src/com/springdemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		Coach b1 = new Baseballcoach();
+		System.out.println(b1.getDailyWorkout());
+
+	}
+
+}
